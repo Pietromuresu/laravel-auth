f24f1219e055fb55694409987ddfc3289e0a1584
Pietromuresu <muresupietro66@gmail.com>
2023-06-15 16:46:57 +0200

    Guest home logged or not

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,10 +1,16 @@
-@extends('layouts.app')
+@extends('layouts.guest')
+
+
 @section('content')
 
-<div class="jumbotron p-5 mb-4 bg-light rounded-3">
-    <div class="container py-5">
+<div class="jumbotronbg-light rounded-3">
+    <div class="container d-flex align-items-center justify-content-center vh-100">
+        @guest
+        <h1 class="text-danger">Guest Home (not logged in)</h1>
+        @else
+        <h1 class="text-success">Guest Home ( logged in)</h1>
 
-        <h1>Ok Sei Arrivato</h1>
+        @endguest
 
     </div>
 </div>
